1c26500ceaa18317a90d7e3c9128d450415f73f6
nbietry <nicolas.bierty@loccitane.com>
2022-12-22 22:35:05 +0100

    ChevauxDeCourses 100%

---
+++ src/main/kotlin/ChevauxDeCourses.kt
@@ -0,0 +1,30 @@
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.abs
+
+fun main() {
+
+    val program = ChevauxDeCourses()
+    program.run()
+}
+
+class ChevauxDeCourses {
+
+    fun run() {
+        val input = Scanner(System.`in`)
+        val N = input.nextInt()
+        var puissanceList = ArrayList<Int>(N)
+        for (i in 0 until N) {
+           puissanceList.add(input.nextInt())
+        }
+        puissanceList.sort()
+        val puissanceEcart = puissanceList.indices.map{i->abs(puissanceList[i] - puissanceList.getOrElse(i+1)
+        { 0
+        }) }
+        debug("$puissanceList $puissanceEcart")
+        println(puissanceEcart.min().toString())
+    }
+
+    private fun debug(message: String){System.err.println(message)}
+
+}
