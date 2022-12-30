25888d32baf9f4478fba1b4928ddfcf46f6d3ee4
nbietry <nicolas.bierty@loccitane.com>
2022-12-30 18:57:39 +0100

    MIME Type - 100%

---
+++ src/main/kotlin/MIMEType.kt
@@ -0,0 +1,30 @@
+import java.util.*
+import java.io.*
+import java.math.*
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fun main(args : Array<String>) {
+    val input = Scanner(System.`in`)
+    val setSize = input.nextInt() // Number of elements which make up the association table.
+    val fileSize = input.nextInt() // Number Q of file names to be analyzed.
+
+    val setOfExtention = hashMapOf<String, String>()
+
+
+    for (i in 0 until setSize) {
+        setOfExtention[input.next().lowercase(Locale.getDefault())] = input.next()
+    }
+    System.err.println(setOfExtention.toString())
+    input.nextLine()
+    for (i in 0 until fileSize) {
+        val FNAME = input.nextLine() // One file name per line.
+        var result = FNAME.split(".").lastOrNull()?.let { setOfExtention.getOrDefault(it.lowercase(Locale.getDefault()), "UNKNOWN") }
+        if(result == null || FNAME.split(".").size < 2) result = "UNKNOWN"
+        System.err.println(FNAME + "=" + result)
+        println(result)
+    }
+
+}
